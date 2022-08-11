9dfa43a273470e68a2419d931ac3d0e2ccc4ab5d
PALOMA AZEVEDO SANTOS <paloma.asantos17@senacsp.edu.br>
2022-08-10 22:12:53 -0300

    Codigo sem return

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Animais
 
             EscolhaAnimal:
             Console.WriteLine("--- Tabela de animais ---");
-            Console.WriteLine("Digite:");
+            Console.WriteLine("-------- Digite: --------");
             Console.WriteLine("(1) para Cachorro");
             Console.WriteLine("(2) para Gato");
             Console.WriteLine("(3) para Cavalo");
@@ -23,15 +23,15 @@ namespace Animais
             switch (animalEscolhido)
             {
                 case "1":
-                    Console.WriteLine("Você escolheu Cachorro");
+                    Console.WriteLine("--> Você escolheu Cachorro");
                     break;
 
                 case "2":
-                    Console.WriteLine("Você escolheu Gato");
+                    Console.WriteLine("--> Você escolheu Gato");
                     break;
 
                 case "3":
-                    Console.WriteLine("Você escolheu Cavalo");
+                    Console.WriteLine("--> Você escolheu Cavalo");
                     break;
 
                 default:
@@ -44,19 +44,19 @@ namespace Animais
             if(animalEscolhido == "1")
             {
                 OpcoesCachorro:
-                Console.Write("Digite: (A) para Andar ou (L) para Latir: ");
+                Console.Write("-- Digite: (A) para Andar ou (L) para Latir: ");
                 string acaoEscolhidaC = Console.ReadLine();
 
                 if(acaoEscolhidaC.ToUpper() == "A")
                 {
-                    Console.Write("Quantas vezes o cachorro irá andar?: ");
+                    Console.Write("-- Quantas vezes o cachorro irá andar?: ");
                     string QuantidadePassos = Console.ReadLine();
                     Cachorro1.AndarCachorro(QuantidadePassos);
                 }
 
                 else if(acaoEscolhidaC.ToUpper() == "L")
                 {
-                    Console.Write("Quantas vezes o cachorro irá latir?: ");
+                    Console.Write("-- Quantas vezes o cachorro irá latir?: ");
                     string QuantidadeLatido = Console.ReadLine();
                     Cachorro1.LatirCachorro(QuantidadeLatido);
                 }
@@ -71,20 +71,20 @@ namespace Animais
             else if(animalEscolhido == "2")
             {   
                 OpcoesGato:
-                Console.Write("Digite: (A) para Andar ou (M) para Miar: ");
+                Console.Write("-- Digite: (A) para Andar ou (M) para Miar: ");
                 string acaoEscolhidaG = Console.ReadLine();
 
                
                 if (acaoEscolhidaG.ToUpper() == "A")
                 {
-                    Console.Write("Quantas vezes o gato irá andar?: ");
+                    Console.Write("-- Quantas vezes o gato irá andar?: ");
                     string QuantidadePassos = Console.ReadLine();
                     Gato1.AndarGato(QuantidadePassos);
                 }
 
                 else if (acaoEscolhidaG.ToUpper() == "M")
                 {
-                    Console.Write("Quantas vezes o gato irá miar?: ");
+                    Console.Write("-- Quantas vezes o gato irá miar?: ");
                     string QuantidadeMiado = Console.ReadLine();
                     Gato1.MiarGato(QuantidadeMiado);
                 }
@@ -98,13 +98,13 @@ namespace Animais
 
             else if(animalEscolhido == "3")
             {
-                Console.Write("Digite: (A) para Andar ou (R) para Relinchar: ");
+                Console.Write("-- Digite: (A) para Andar ou (R) para Relinchar: ");
                 string acaoEscolhidaV = Console.ReadLine();
 
                 OpcoesCavalo:
                 if (acaoEscolhidaV.ToUpper() == "A")
                 {
-                    Console.Write("Quantas vezes o cavalo irá andar?: ");
+                    Console.Write("-- Quantas vezes o cavalo irá andar?: ");
                     string QuantidadePassos = Console.ReadLine();
 
                     Cavalo1.AndarCavalo(QuantidadePassos);
@@ -112,7 +112,7 @@ namespace Animais
 
                 else if (acaoEscolhidaV.ToUpper() == "R")
                 {
-                    Console.Write("Quantas vezes o cavalo irá relinchar?: ");
+                    Console.Write("-- Quantas vezes o cavalo irá relinchar?: ");
                     string QuantidadeRe = Console.ReadLine();
                     Cavalo1.ReCavalo(QuantidadeRe);
                 }
